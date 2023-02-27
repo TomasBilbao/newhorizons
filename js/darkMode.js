7e1dcb6862d3a5a0cee1ad38fcec788767a1dc5a
“Tomas <tomasbilbao05@gmail.com>
2023-02-27 20:48:01 -0300

    agregue img y codigo

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,29 +1,35 @@
+// Capturas botones Dark Mode:
+
 let botonDarkMode = document.getElementById("botonDarkMode")
 let botonLightMode = document.getElementById("botonLightMode")
-let eliminarModeBtn = document.getElementById("eliminarMode")
 
-let modoOscuro 
+// Eventos para botones de Dark Mode y localStorage:
 
-if(localStorage.getItem("modoOscuro")){
-    modoOscuro = JSON.parse(localStorage.getItem("modoOscuro"))
-}else{
-    localStorage.setItem("modoOscuro", false)
-    modoOscuro = JSON.parse(localStorage.getItem("modoOscuro"))
-} 
-
-if(modoOscuro == true){
+botonDarkMode.addEventListener("click", () => {
     document.body.classList.add("darkMode")
-}else{
-    document.body.classList.remove("darkMode")
-}
-
-botonDarkMode.addEventListener("click",()=>{
-    document.body.classList.add("darkMode")
-
     localStorage.setItem("modoOscuro", true)
 })
 
-botonLightMode.addEventListener("click",()=>{
+botonLightMode.addEventListener("click", () => {
     document.body.classList.remove("darkMode")
     localStorage.setItem("modoOscuro", false)
 })
+
+// Condicional para la primera vez que se entra al sitio:
+
+let modoOscuro
+
+if (localStorage.getItem("modoOscuro")) {
+    modoOscuro = JSON.parse(localStorage.getItem("modoOscuro"))
+} else {
+    localStorage.setItem("modoOscuro", false)
+    modoOscuro = JSON.parse(localStorage.getItem("modoOscuro"))
+}
+
+// Dato Bandera:
+
+if (modoOscuro == true) {
+    document.body.classList.add("darkMode")
+} else {
+    document.body.classList.remove("darkMode")
+}
